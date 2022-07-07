731bef77af373fd05c25d8a6860e537c9776ad6b
Karun Atreya <karunat1999@gmail.com>
2022-07-07 23:44:18 +0545

    added searching to user profile api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,11 +1,29 @@
-from unicodedata import name
-from unittest.util import _MAX_LENGTH
+from profiles_api import models
 from rest_framework import serializers
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIview"""
     name = serializers.CharField(max_length=10)
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes user profile object"""
+    class Meta:
+        model=models.UserProfile
+        fields=('id', 'email', 'name', 'password')
+        extra_kwargs= {
+            'password':{
+                'write_only': True,
+                'style': {'input_type': 'password'},
+            }
+        }
     
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user= models.UserProfile.objects.create_user(
+            email= validated_data['email'],
+            name= validated_data['name'],
+            password= validated_data['password'],
+        )
 
+        return user
 
